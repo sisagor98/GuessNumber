f90e8f14c6e9e3f0c0f573ff41ab8f1ff5232e18
shariful islam <sisagor98@gmail.com>
2019-12-16 22:22:17 +0600

    Complete Game

---
+++ .history/Assets/Scripts/GameContoller1_20191214133404.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//using System;
+
+public class GameContoller1 : MonoBehaviour
+{
+    private int num;
+    private int guessNumber;
+    private int countGuess;
+
+    [SerializeField]
+    private Text text;
+    [SerializeField]
+    private InputField input;
+
+   // public InputField input;
+    private void Awake() {
+        num = Random.Range(0, 100);
+        text.text = "Guess A Number Between 0 to 100";
+    }
+   public void GetInput(string guessvalu){
+       Debug.Log ("You Entered" + guessvalu);
+      // CompareGuess(guess);
+      // CompareGuess(Convert.ToInt32(guess));
+       //CompareGuess(int.TryParse(guess));
+       //CompareGuess(int.Parse(guess));
+      // CompareGuess(System.Convert.ToInt32(guess));
+       input.text = "";
+
+   }
+
+//    void CompareGuess(string guessvalu){
+       
+//        guess = Convert.ToInt32(guessvalu);
+//        if(guess  == num){
+//            text.text = "You Gyessed Correctly The Number Was" + guess;
+//        }
+//        else if(guess < num){
+//            text.text ="Your Guess Number is Less Then The Number You Trying Again To Guess";
+//        }
+//        else if(guess > num){
+//            text.text ="Your Guess Number is Greather Then The Number You Trying Again To Guess";
+//         }
+//    }
+}
+
---
+++ Assets/Scripts/GameContoller1.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//using System;
+
+public class GameContoller1 : MonoBehaviour
+{
+    private int num;
+    private int guessNumber;
+    private int countGuess =0;
+
+    [SerializeField]
+    private Text text;
+    [SerializeField]
+    private InputField input;
+
+    public GameObject playAgain;
+    public GameObject restart;
+
+   // public InputField input;
+    private void Awake() {
+        num = Random.Range(0, 100);
+        text.text = "Guess A Number Between 0 to 100";
+        
+    }
+   public void GetInput(string guess){
+       //Debug.Log ("You Entered" + guess);
+       CompareGuess(int.Parse(guess));
+       input.text = "";
+       countGuess++;
+       restart.SetActive(true);
+
+
+   }
+
+   void CompareGuess(int guess){
+       
+      // guess = Convert.ToInt32(guessvalu);
+       if(guess  == num){
+           text.text = "You Gyessed Correctly The Number. "+ guess +"Your tried"+countGuess;
+       }
+       else if(guess < num){
+           text.text ="Your Guess Number is Less Then The Number. You Trying Again To Guess";
+       }
+       else if(guess > num){
+           text.text ="Your Guess Number is Greather Then The Number.You Trying Again To Guess";
+        }
+   }
+
+   public void PlayAgain(){
+       num = Random.Range(0,100);
+       text.text = "Guess A Number Between 0 to 100";
+       countGuess =0;
+       playAgain.SetActive(false);
+
+   }
+}
+
